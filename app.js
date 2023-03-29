d275d281e7e984e23f67debe48155e229898eeef
AMQIsmail <109195072+AMQIsmail@users.noreply.github.com>
2023-03-29 09:15:47 +0600

    Update app.js
    Ert

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res){
 res.send('node js run with mobile 28');
 });
 
+// undefine route
+app.use('*', function(req, res){
+   res.send("404");
+});
+
 
 app.listen(300, function(){
  console.log('node js runing with mobile');
