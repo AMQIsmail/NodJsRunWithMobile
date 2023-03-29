87e634070ed3c90fff7d3ae468ac872986a75064
AMQIsmail <109195072+AMQIsmail@users.noreply.github.com>
2023-03-29 09:36:45 +0600

    Update app.js
    U

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 
 const express  = require('express');
-const app = express();
+const app = new express();
 // requure api
 //const api = require('./src/routes/api');
 
@@ -13,15 +13,17 @@ const app = express();
 //app.use('/', api);
 //app.use('/api/v1', api);
 
-// undefine route
-app.use('*', function(req, res){
-   res.send("404");
-});
-
 // home route
 app.get('/', function(req, res){
   res.send("hello home route 28");
 });
+
+// undefine route
+app.use('*', function(req, res){
+   res.send("404 ok err 28");
+});
+
+
 
 // app export
 module.exports = app
