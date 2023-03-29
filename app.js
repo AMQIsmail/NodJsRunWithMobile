2300dd18ef9aac31c6fcc8999611b7dec4a119a5
AMQIsmail <109195072+AMQIsmail@users.noreply.github.com>
2023-03-29 09:20:02 +0600

    Update app.js
    New

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const app = new express();
 
 
-app.get('/', function(req, res){
-res.send('node js run with mobile 28');
-});
+
+ 
+
+
+const express  = require('express');
+const app = express();
+// requure api
+const api = require('./src/routes/api');
+
+// versioning api
+app.use('/', api);
+app.use('/api/v1', api);
 
 // undefine route
 app.use('*', function(req, res){
    res.send("404");
 });
 
+// home route
+app.get('/', function(req, res){
+  res.send("hello home route");
+});
 
-app.listen(300, function(){
- console.log('node js runing with mobile');
-})
-
+// app export
+module.exports = app
