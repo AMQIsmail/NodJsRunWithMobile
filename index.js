b456e7da03175872a04d53cae24956312ab9cbe7
AMQIsmail <109195072+AMQIsmail@users.noreply.github.com>
2023-03-29 09:26:44 +0600

    Create index.js
    Create index file

---
+++ index.js
@@ -0,0 +1,14 @@
+// app require
+const app = require("./app");
+
+// dotenv Implement
+const dotenv = require("dotenv");
+dotenv.config({path:"./config.env"});
+// Port number
+const Port = process.env.PORT;
+
+// app listen 
+app.listen(Port, function() {
+    console.log(`Server Runing PORT ${Port}`);
+});
+
